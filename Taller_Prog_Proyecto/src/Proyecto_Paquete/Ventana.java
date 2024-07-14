dc9c9eacecf96663ea5f9851b4e7e3b91c62cd1f
Angel-dt1511 <angel.vdx76@gmail.com>
2024-07-13 21:35:54 -0500

    Subida del archivo java

---
+++ Taller_Prog_Proyecto/src/Proyecto_Paquete/Ventana.java
@@ -0,0 +1,10 @@
+
+package Proyecto_Paquete;
+
+public class Ventana {
+
+    public static void main(String[] args) {
+        // Si ves esto signficia que haz logrado abrir el archivo java correctamente :>
+    }
+    
+}
